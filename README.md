7ff40745836aeb5ec69d9d0e6657f62c0c20c0d0
beltransitooo <migzaz20005@hotmail.com>
2026-08-05 23:39:12 -0700

    Inicio del proyecto con fichero main

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Plataforma-de-blogs-
+
+POST: Para crear datos
+GET: Para leer datos
+PUT: Para actualizar datos
+DELETE: Para borrar datos
--- backend/main.py
+++ backend/main.py
@@ -5,11 +5,3 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return "Hello World"
-
-@app.get("/prueba")
-async def prueba():
-    return "Mensaje de prueba"
-
-@app.get("/pruebaSanti")
-async def pruebaSanti():
-    return "Mensaje de prueba al Santi"
