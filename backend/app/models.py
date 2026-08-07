3a63883c6d664723a90f14b8690528a0110d3947
beltransitooo <migzaz20005@hotmail.com>
2026-08-06 23:03:16 -0700

    Creacion de tablas en la base de datos

---
+++ backend/app/models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String, Text, Boolean, DateTime, ForeignKey
+from sqlalchemy.rom import relationship
+from datetime import datetime
+from .database import Base
+
+class Usuario(Base):
+    __tablename__ = "usuarios"
+    id = Column(Integer, primary_key=True, index=True)
+    nombre_usuario = Column(String, unique=True, index=True, nullable=False)
+    correo = Column(String, unique=True, index=True, nullable=False)
+    contraseña_hash = Column(String, nullable=False)
+
+    publicaciones = relationship("Publicacion", back_populates="propietario", cascade="all, delete-orphan")
+
+class Publicaciones(Base):
+    __tablename__ = "publicaciones"
+    id = Column(Integer, primary_key=True, index=True)
+    titulo = Column(String, nullable=False)
+    contenido = Column(Text, nullable=False)
+    creado_en = Column(DateTime, default=datetime.utcnow)
+    id_propietario = Column(Integer, ForeignKey("usuarios.id"), nullable=False)
+
+    publicaciones = relationship("Usuario", back_populates="publicaciones")
