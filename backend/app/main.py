a2f6169537e8f1386cae5d2244b511c76d3dd5c8
beltransitooo <migzaz20005@hotmail.com>
2026-08-06 23:15:31 -0700

    Modelado inicial del main con creacion de tablas, inicializar fastapi y ruta de prueba

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from .database import engine
+from . import models
 
-app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI(
+    titulo="API de mi plataforma de Blogs",
+        version="1.0.0"
+)
 
 @app.get("/")
 async def root():
-    return "Hello World"
+    return "Bienvenido a la API de mi Blog funcional al 100"
